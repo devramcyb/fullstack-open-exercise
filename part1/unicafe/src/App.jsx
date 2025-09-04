a10fe4561c5f90cc65965b21b6a4d7be60be83e1
Ramdev sarswat <ramdevsarswat1@gmail.com>
2025-09-04 11:48:02 +0530

    Add unicafe average,positivity

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,8 +1,8 @@
 import { useState } from "react";
 
-// const Button = (props) => {
-//   return <button onclick={props.onclick}>{props.text}</button>;
-// };
+const Button = (props) => {
+  return <button onClick={props.onClick}>{props.text}</button>;
+};
 
 const App = () => {
   // save clicks of each button to its own state
@@ -10,25 +10,39 @@ const App = () => {
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
 
-  console.log(good, neutral, bad);
+  // Calculate total from current state values
+  const all = good + neutral + bad;
+  const average = (good - bad) / all;
+  const positive = (good * 100) / all;
+
+  function setGoodReview() {
+    setGood(good + 1);
+  }
+
+  function setNeutralReview() {
+    setNeutral(neutral + 1);
+  }
+
+  function setBadReview() {
+    setBad(bad + 1);
+  }
 
   return (
     <div>
       <h1>Give Feedback</h1>
-      <button
-        onClick={() => {
-          setGood(good + 1);
-        }}
-      >
-        good
-      </button>
-      <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
-      <button onClick={() => setBad(bad + 1)}>bad</button>
 
-      <h1>statics</h1>
+      <Button onClick={setGoodReview} text="good" />
+      <Button onClick={setNeutralReview} text="neutral" />
+      <Button onClick={setBadReview} text="bad" />
+
+      <h1>Statistics</h1>
+
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
+      <p>all {all}</p>
+      <p>average {average}</p>
+      <p>positive {positive}%</p>
     </div>
   );
 };
